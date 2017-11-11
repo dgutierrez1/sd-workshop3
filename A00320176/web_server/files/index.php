a37d77e66a8cbc741865f839dad238acf85dc951
root <d.aguti@hotmail.com>
2017-11-11 14:59:59 +0000

    final commit

--- A00320176/web_server/files/index.php
+++ A00320176/web_server/files/index.php
@@ -1,5 +1,7 @@
 <?php
- 
+ echo "<h1> Taller 3 - Daniel Gutierrez</h1><br>"
+ echo "<h5>Listado de cursos</h5><br>" 
+
  $con = new PDO('mysql:host=25.8.240.42;dbname=db_dist;charset=utf8mb4', 'admin', 'password');
 if (!$con)
   {
